0bba4b9f12b22b377a35e2acf0f226785bc75c0b
Udara <it25100714@my.sliit.lk>
2026-05-18 15:05:55 +0530

    final debug and test

--- src/main/java/org/example/taxiadmin/controller/HomeController.java
+++ src/main/java/org/example/taxiadmin/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.TaxiBookingSystem.demo;
+package org.example.taxiadmin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
